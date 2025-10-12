fb0b64c851430a31745af5b42741ad78d6b7dfa2
kptudyogamela <kptudyogamela@gmail.com>
2025-10-12 11:29:39 +0530

    after ROI fix

--- frontend/src/app/admin/add-fd/page.jsx
+++ frontend/src/app/admin/add-fd/page.jsx
@@ -158,19 +158,19 @@ export default function AdminFDForm() {
               className="mt-1 block w-full border border-gray-300 rounded-xl px-3 py-2 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             >
               <option value="">-- Select Rate --</option>
-              <option value="6.0">4.0%</option>
-              <option value="6.0">4.5%</option>
-              <option value="6.0">5.0%</option>
-              <option value="6.0">5.5%</option>
+              <option value="4.0">4.0%</option>
+              <option value="4.5">4.5%</option>
+              <option value="5.0">5.0%</option>
+              <option value="5.5">5.5%</option>
               <option value="6.0">6.0%</option>
               <option value="6.5">6.5%</option>
               <option value="7.0">7.0%</option>
-              <option value="7.0">7.5%</option>
-              <option value="7.0">8.0%</option>
-              <option value="7.0">8.5%</option>
-              <option value="7.0">9.0%</option>
-              <option value="6.0">9.5%</option>
-              <option value="6.0">10.0%</option>
+              <option value="7.5">7.5%</option>
+              <option value="8.0">8.0%</option>
+              <option value="8.5">8.5%</option>
+              <option value="9.0">9.0%</option>
+              <option value="9.5">9.5%</option>
+              <option value="10.0">10.0%</option>
             </select>
           </div>
 
